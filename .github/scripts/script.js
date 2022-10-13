e97ef72c948e61e6611d1b6e38c5fccaec32bbd3
Thomas Dupoiron <tdupoiron@github.com>
2022-10-13 11:21:09 +0200

    Update script.js

--- .github/scripts/script.js
+++ .github/scripts/script.js
@@ -1,6 +1,11 @@
+const { Octokit } = require("@octokit/rest");
+const github = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+});
+
 github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
     body: "🎉 You've created this issue comment using GitHub Script!!!"
-  })
+})
